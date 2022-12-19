52537b28af8b512689f72e7b43f75437967cfc51
Matthew Miller <39663150+python151@users.noreply.github.com>
2022-12-18 22:33:08 -0600

    Rudimentary UI Framework

---
+++ Assets/SettingsInputComponent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+public class SettingsInputComponent : MonoBehaviour
+{
+    public GameObject inputObject;
+    public void ValueChanged()
+    {
+        return;
+    }
+}
---
+++ Assets/VolumeInputSetting.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Slider = UnityEngine.UI.Slider;
+
+public class VolumeInputSetting : SettingsInputComponent
+{
+    public void ValueChanged()
+    {
+        GlobalSettings.audioVolume = inputObject.GetComponent<Slider>().value;
+        Debug.Log(GlobalSettings.audioVolume);
+    }
+}
