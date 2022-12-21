3eaf3c45e8f6cfaab2527b97a2b647f56cf42e52
Matthew Miller <39663150+python151@users.noreply.github.com>
2022-12-21 11:31:50 -0600

    Added Difficulty Setting + Refactoring
    Switched the ScoreBoard and Timer UI GameObjects to be Prefabs

--- Assets/DictionaryScript.cs
+++ Assets/DictionaryScript.cs
@@ -1,11 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 public class DictionaryScript : MonoBehaviour
 {
+    public static int difficulty { get; private set; } = 0;
     public static string getWordOrPhrase()
     {
-        return "Memorization";
+        // TODO: Make this more dynamic, maybe include an API --- Or at least a much longer word list!
+
+        switch (difficulty)
+        {
+            case 0:
+                return "Apple";
+            case 1:
+                return "Certified";
+            case 2:
+                return "Incomprehensibleness";
+            default:
+                throw new KeyNotFoundException("Difficulty variable set incorrectly!");
+                return "Apple";
+        }
+    }
+
+    public static void setDifficulty(int updated_difficulty)
+    { 
+        difficulty = updated_difficulty;
+        Debug.Log("Difficulty changed to " + difficulty);
     }
 }
